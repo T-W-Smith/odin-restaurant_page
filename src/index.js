b7da50949ea951db34f5afc0cd0a4be8edf60d50
Trey Smith <slicksmith14@gmail.com>
2024-07-25 17:58:02 -0400

    Add nav bar tab functionality

--- src/index.js
+++ src/index.js
@@ -2,4 +2,23 @@ import {loadHome} from './homeload';
 import { loadMenu } from './menuload';
 import { loadContact } from './contactload';
 
-window.onload(loadHome());
+window.onload = loadHome();
+
+const navBar = (function() {
+    const homeBtn = document.getElementById('home');
+    const menuBtn = document.getElementById('menu');
+    const contectBtn = document.getElementById('contact');
+
+    homeBtn.addEventListener('click', function() {
+        document.getElementById('content').replaceChildren();
+        loadHome();
+    });
+    menuBtn.addEventListener('click', function() {
+        document.getElementById('content').replaceChildren();
+        loadMenu();
+    });
+    contectBtn.addEventListener('click', function() {
+        document.getElementById('content').replaceChildren();
+        loadContact();
+    });
+})();
